5538eeae046ebcdd1999bd1e6cb60287ef1fab82
Ibnuubaidillah1009 <ibnuubaidillah1009@gmail.com>
2025-02-03 14:23:44 +0700

    hasil.php

--- hasil.php
+++ hasil.php
@@ -10,37 +10,40 @@
 <form>
         <h1>Hasil Perhitungan</h1>
     <?php
+    if (isset($_GET['angka1'], $_GET['angka2'], $_GET['operator'])) {
         $angka1 = $_GET['angka1'];
         $angka2 = $_GET['angka2'];
         $operator = $_GET['operator'];
 
-        if (empty($angka1) || empty($angka2)) {
-            echo "Error: masukkan angka yang benar!";
-        } else {
-            if($operator == '+') {
-                $hasil = $angka1 + $angka2;
-            } elseif($operator == '-') {
-                $hasil = $angka1 - $angka2;
-            } elseif($operator == ':') {
-                if ($angka2 == 0) {
-                    echo "Error: masukkan angka yang benar!";
-                } else {
-                    $hasil = $angka1 / $angka2;
-                }
-            } elseif($operator == 'x') {
-                $hasil = $angka1 * $angka2;
-            } elseif ($operator == '^') {
-                $hasil = $angka1 ** $angka2;
-            } elseif ($operator == '%') {
-                $hasil = $angka1 % $angka2;
-            }
-            if ($operator != ':') {
+        if ($angka1 == "" || $angka2 == "") {
+            echo "Error: Harap masukkan kedua angka!";
+        } elseif($operator == '+') {
+            $hasil = $angka1 + $angka2;
+            echo $angka1 . $operator . $angka2 . '=' . $hasil;
+        } elseif($operator == '-') {
+            $hasil = $angka1 - $angka2;
+            echo $angka1 . $operator . $angka2 . '=' . $hasil;
+        } elseif($operator == ':') {
+            if ($angka2 == 0) {
+                echo "Error: Maaf input salah";
+            } else {
+                $hasil = $angka1 / $angka2;
                 echo $angka1 . $operator . $angka2 . '=' . $hasil;
             }
+        } elseif($operator == 'x') {
+            $hasil = $angka1 * $angka2;
+            echo $angka1 . $operator . $angka2 . '=' . $hasil;
+        } elseif ($operator == '^') {
+            $hasil = $angka1 ** $angka2;
+            echo $angka1 . $operator . $angka2 . '=' . $hasil;
+        } elseif ($operator == '%') {
+            $hasil = $angka1 % $angka2;
+            echo $angka1 . $operator . $angka2 . '=' . $hasil;
+        }
         }
     ?>
     <a href="index.php">Kembali</a>
     <a type="button" onclick="return window.print();">Cetak</a>
-    </form>
+</form>
 </body>
 </html>
